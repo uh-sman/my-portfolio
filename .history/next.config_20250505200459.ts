90c38fe06627a79209b82a8bcc1b215806846d73
uh-sman <uumar7000@gmail.com>
2025-05-05 20:06:09 +0100

    added ignoreBuildError flag

--- .history/next.config_20250505200459.ts
+++ .history/next.config_20250505200459.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript:{
     ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-    
   }
 };
 
--- .history/next.config_20250505200501.ts
+++ .history/next.config_20250505200501.ts
@@ -4,11 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript:{
     ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-    
   }
+  
 };
 
 export default nextConfig;
